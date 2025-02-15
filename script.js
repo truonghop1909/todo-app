331e5e86a5c2b3b7cd614e6ca183bc4ec0334317
unknown <truonghop1909@gmail.com>
2025-02-16 00:14:24 +0700

    Cập nhật thông báo hỏi

--- script.js
+++ script.js
@@ -44,8 +44,122 @@ const showAlert = (content = null, time = 3000) => {
     }
     
 }
-    
 // Hết Hiển thị thông báo
+
+const closeModal = (element) => {
+    const body = document.querySelector("body");
+
+    const modalClose = element.querySelector(".modal__close");
+    const modalOverlay = element.querySelector(".modal__overlay");
+    const buttonClose = element.querySelector(".button__close");
+
+    buttonClose.addEventListener("click", () => {
+        body.removeChild(element);
+    })
+
+    modalClose.addEventListener("click", () => {
+        body.removeChild(element);
+    })
+
+    modalOverlay.addEventListener("click", () => {
+        body.removeChild(element);
+    })
+}
+
+// Hiển thị comfirm 
+const showConfirmDelete = (id) => {
+    const body = document.querySelector("body");
+
+    const elementConfirm = document.createElement("div");
+    elementConfirm.classList.add("modal");
+
+    elementConfirm.innerHTML = `
+        <div class="modal__main">
+            <button class="modal__close">
+                <i class="fa-solid fa-x"></i>
+            </button>
+            <div class="modal__content">
+                <div class="modal__text">
+                    Bạn có chắc chắn muốn xóa công việc này ?
+                </div>
+                <div class ="modal__item-actions">
+                    <button class="button__close">Hủy</button>
+                    <button class="button__agree">Đồng Ý</button>
+                </div>
+            </div>
+        </div>
+        <div class="modal__overlay"></div>
+    `;
+
+    body.appendChild(elementConfirm);
+
+    closeModal(elementConfirm);
+
+    const buttonAgree = elementConfirm.querySelector(".button__agree");
+    buttonAgree.addEventListener("click", () => {
+        remove(ref(db, '/todos/' + id)).then(() => {
+            body.removeChild(elementConfirm);
+            showAlert("Xóa thành công!", 3000);
+        });
+    })
+
+    
+}
+// Hết Hiển thị comfirm 
+
+// Hiển thị edit
+
+const showConfirmEdit = (id) => {
+    const body = document.querySelector("body");
+
+    const elementEdit = document.createElement("div");
+    elementEdit.classList.add("modal");
+
+    elementEdit.innerHTML = `
+        <div class="modal__main">
+            <button class="modal__close">
+                <i class="fa-solid fa-x"></i>
+            </button>
+            <div class="modal__content">
+                <div class="modal__text">
+                    Chỉnh sửa công việc ?
+                </div>
+                <input class="modal__input" name="content"/>
+                <div class="modal__item-actions">
+                    <button class="button__close">Hủy</button>
+                    <button class="button__agree">Thay đổi</button>
+                </div>      
+            </div>
+        </div>
+        <div class="modal__overlay"></div>
+    `;
+
+    body.appendChild(elementEdit);
+
+    closeModal(elementEdit);
+
+    const buttonAgree = elementEdit.querySelector(".button__agree");
+    buttonAgree.addEventListener("click", () => {
+        const content = elementEdit.querySelector("input[name='content']").value;
+        if(content) {
+            const dataUpdate = {
+                content: content
+            };
+            update(ref(db, 'todos/' + id), dataUpdate).then(() => {
+                body.removeChild(elementEdit);
+                showAlert("Cập nhật thành công !", 3000);
+            });
+        }
+    })
+    onValue(ref(db, '/todos/' + id), (item) => {
+        const data = item.val();
+        elementEdit.querySelector("input[name='content']").value = data.content;
+    })
+}
+// Hết Hiển thị edit
+
+
+
 
 // Thêm công việc
 const todoAppCreate = document.querySelector("#todo-app-create");
@@ -102,7 +216,9 @@ onValue(todosRef, (items) => {
             <div class="todo-app__item ${data.complete ? 'todo-app__item--complete' : ''}">
                 <span class="todo-app__item-content">${data.content}</span>
                 <div class="todo-app__item-actions">
-                    <button class="todo-app__item-button todo-app__item-button--edit">
+                    <button
+                        class="todo-app__item-button todo-app__item-button--edit"
+                        button-edit="${key}">
                         <i class="fa-solid fa-pen-to-square"></i>
                     </button>
                     ${ButtonComplete}
@@ -151,35 +267,31 @@ onValue(todosRef, (items) => {
         })
     })
 
+    // Hết Tính năng hoàn tác công việc 
+
     // Tính năng xóa công việc
     const listButtonRemove = document.querySelectorAll("[button-remove]");
     listButtonRemove.forEach(button => {
         button.addEventListener("click", () => {
             const id = button.getAttribute("button-remove");
-            remove(ref(db, '/todos/' + id)).then(() => {
-                showAlert("Xóa thành công!", 3000);
-            });
+            showConfirmDelete(id);
         })
     })
     
     // Hết Tính năng xóa công việc
+
+    // Tính năng chỉnh sửa công việc
+    const listButtonEdit = document.querySelectorAll("[button-edit]");
+    listButtonEdit.forEach(button => {
+        button.addEventListener("click", () => {
+            const id = button.getAttribute("button-edit");
+
+            showConfirmEdit(id);
+        })
+    })
+    // Hết Tính năng chỉnh sửa công việc
     
-    // Hết Tính năng hoàn tác công việc 
-
-    // Cập nhật công việc
-    // const buttonUpdate = document.querySelector(".button-update");
-    // buttonUpdate.addEventListener("click", () => {
-    //     const id = "-OIoRhaYP9saIPlE-PKS";
-    //     const dataUpdate = {
-    //         fullName: "Le Van B"
-    //     };
-    //     update(ref(db, '/users/' + id), dataUpdate);
-    // })
-
-    // Hết cập nhật công việc
+    
 })
 
 //Hết Lấy danh sách công việc
-
-
-
